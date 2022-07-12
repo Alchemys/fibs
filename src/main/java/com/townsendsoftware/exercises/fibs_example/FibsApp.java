aa4feed8d59539cc86cbad6909f4d287ec02710f
Jeff Jones <alchemys@gmail.com>
2022-07-11 22:31:10 -0600

    Added logback and lombok

--- src/main/java/com/townsendsoftware/exercises/fibs_example/FibsApp.java
+++ src/main/java/com/townsendsoftware/exercises/fibs_example/FibsApp.java
@@ -1,6 +1,9 @@
 package com.townsendsoftware.exercises.fibs_example;
 
 import com.townsendsoftware.exercises.fibs_example.utils.LogUtils;
+
+import lombok.extern.slf4j.Slf4j;
+
 import com.townsendsoftware.exercises.fibs_example.cli.CliProcessor;
 import com.townsendsoftware.exercises.fibs_example.generators.GenerateFibonacciSequence;
 import java.util.List;
@@ -19,6 +22,7 @@ This code is simply an experiment and is intended for educational purposes only.
 That being said, if you wish to use this code in part or in whole, please acknowledge the original author.
 */
 
+@Slf4j
 public class FibsApp {
 
     public static void main(String[] args) {
@@ -36,7 +40,9 @@ public class FibsApp {
             long seqCount = 0;
             for (Long seqVal : sequences) {
               sbForPrint.append(seqVal);
-              sbForPrint.append(", ");
+              if (seqCount < sequences.size()-1) {
+                sbForPrint.append(", ");
+              }
               if (seqCount % 10 == 0 && seqCount > 0) {
                 sbForPrint.append(SYSTEM_LINE_SEP);
               }
@@ -44,15 +50,15 @@ public class FibsApp {
             }
             LogUtils.logMessage(String.format("%sRequested sequence:%s%s%s", SYSTEM_LINE_SEP, SYSTEM_LINE_SEP, sbForPrint.toString(), SYSTEM_LINE_SEP));
           } else {
-            LogUtils.logMessage("Failed to generate a sequence.");
+            log.info("Failed to generate a sequence.");
           }
 
         } else {
-            LogUtils.logMessage(String.format("Provided iteration count [%s], is out of range.  Must be between 1 and 50", cliConfig.iterationCount));
+            log.info("Provided iteration count [{}], is out of range.  Must be between 1 and 50", cliConfig.iterationCount);
         }
 
       } else {
-        LogUtils.logMessage("ERROR: Failed to parse command line arguments.");
+        log.error("Failed to parse command line arguments.");
       }
     }
 }
--- src/main/java/com/townsendsoftware/exercises/fibs_example/generators/GenerateFibonacciSequence.java
+++ src/main/java/com/townsendsoftware/exercises/fibs_example/generators/GenerateFibonacciSequence.java
@@ -5,15 +5,18 @@ import java.util.List;
 
 import com.townsendsoftware.exercises.fibs_example.utils.LogUtils;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class GenerateFibonacciSequence {
     
     public static List<Long> generateSequence(long seqSeed0, long seqSeed1, long iterations) {
         List<Long> generatedSequence = new ArrayList<Long>();
         if (seqSeed0 >= 0 && seqSeed1 > 0 && iterations > 0) {
             if (seqSeed0 >= seqSeed1 && seqSeed1 != 1 && seqSeed0 !=1) {
-                LogUtils.logMessage(String.format("seqSeed0 must be < seqSeed1 (unless both are 1). Provided: seqSeed0[%d], seqSeed1[%d], iterations[%d]", seqSeed0, seqSeed1, iterations));
+                log.error("seqSeed0 must be < seqSeed1 (unless both are 1). Provided: seqSeed0[{}], seqSeed1[{}], iterations[{}]", seqSeed0, seqSeed1, iterations);
             } else {
-                LogUtils.logMessage(String.format("Generating fibonacci sequence using seeds %d, %d for %d iterations", seqSeed0, seqSeed1, iterations));
+                log.info("Generating fibonacci sequence using seeds {}, {} for {} iterations", seqSeed0, seqSeed1, iterations);
                 generatedSequence.add(seqSeed0);
                 generatedSequence.add(seqSeed1);
                 for (int seqCount = 2; seqCount < iterations-2; seqCount++) {
@@ -23,7 +26,7 @@ public class GenerateFibonacciSequence {
                 } 
             }
         } else {
-            LogUtils.logMessage(String.format("seqSeed0, seqSeed1 and iterations must be positive long values. Provided: seqSeed0[%d], seqSeed1[%d], iterations[%d]", seqSeed0, seqSeed1, iterations));
+            log.error("seqSeed0, seqSeed1 and iterations must be positive long values. Provided: seqSeed0[{}], seqSeed1[{}], iterations[{}]", seqSeed0, seqSeed1, iterations);
         }
 
         return generatedSequence;
